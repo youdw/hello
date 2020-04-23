2fea00e0ae50961a1bd4e675115d0b19e159e648
youdw <youdw@foxmail.com>
2020-04-23 19:56:41 +0800

    v2

--- hello.go
+++ hello.go
@@ -3,9 +3,9 @@ package hello
 import "rsc.io/quote/v3"
 
 func Hello() string {
-    return quote.HelloV3()
+	return quote.HelloV3()
 }
 
 func Proverb() string {
-    return quote.Concurrency()
+	return quote.Concurrency()
 }
